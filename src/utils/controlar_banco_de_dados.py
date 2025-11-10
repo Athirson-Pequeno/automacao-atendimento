d078a325d6c23fd77e0dedcaa60f586f7d5ead4b
Athirson <athirsondev@gmail.com>
2025-11-10 13:53:21 -0300

    Inclusão do requirements.txt

--- src/utils/controlar_banco_de_dados.py
+++ src/utils/controlar_banco_de_dados.py
@@ -2,7 +2,10 @@ import os
 
 import pandas as pd
 import psycopg2
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
+
+load_dotenv()
 
 # Caminho absoluto até a raiz do projeto
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
@@ -13,6 +16,12 @@ RELATORIOS_DIR = os.path.join(BASE_DIR, "relatorios")
 os.makedirs(DATABASE_DIR, exist_ok=True)
 os.makedirs(TABELAS_DIR, exist_ok=True)
 os.makedirs(RELATORIOS_DIR, exist_ok=True)
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 
 def salvarTabela(arquivo):
@@ -31,16 +40,17 @@ def salvarTabela(arquivo):
         "DataÚltimaLeitura": "ultima_leitura",
         "Plataforma": "plataforma",
         "Dias off.": "dias_off",
-        "TipoMedidor": "tipo_medidor"
+        "TipoMedidor": "tipo_medidor",
+        "Manutencao": "manutencao"
     }, inplace=True)
 
     # --- Conexão com o PostgreSQL ---
     conn = psycopg2.connect(
-        host="localhost",  # ou "postgres" se rodar dentro do Docker Compose
-        port="5432",
-        database="atendimento_cliente",
-        user="admin",
-        password="asp36412"
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
     )
     cursor = conn.cursor()
 
@@ -56,6 +66,7 @@ def salvarTabela(arquivo):
             plataforma TEXT,
             status TEXT,
             tipo_medidor TEXT,
+            manutencao TEXT,
             UNIQUE(data_registro, nome, descricao_sensor)
         )
     """)
@@ -66,8 +77,8 @@ def salvarTabela(arquivo):
     for _, row in df.iterrows():
         cursor.execute("""
                 INSERT INTO historico_sensores (
-                    data_registro, nome, descricao_sensor, email, ultima_leitura, plataforma, tipo_medidor, status
-                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                    data_registro, nome, descricao_sensor, email, ultima_leitura, plataforma, tipo_medidor, status, manutencao
+                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
                 ON CONFLICT (data_registro, nome, descricao_sensor) DO NOTHING
             """, (
             row["data_registro"],
@@ -77,7 +88,8 @@ def salvarTabela(arquivo):
             row.get("ultima_leitura", None),
             row.get("plataforma", ""),
             row["tipo_medidor"],
-            "OFF" if row["dias_off"] > 0 else "ON"
+            "OFF" if row["dias_off"] > 0 else "ON",
+            row["manutencao"]
         ))
 
     conn.commit()
@@ -86,7 +98,7 @@ def salvarTabela(arquivo):
     ano = str(pd.Timestamp.today().year)
     mes = str(pd.Timestamp.today().month).zfill(2)
 
-    engine = create_engine("postgresql+psycopg2://admin:asp36412@localhost:5432/atendimento_cliente")
+    engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
     df_mensal = pd.read_sql_query("SELECT * FROM historico_sensores ORDER BY data_registro ASC", engine)
 
     nome_relatorio = os.path.join(RELATORIOS_DIR, f"historico_{ano}_{mes}.xlsx")
